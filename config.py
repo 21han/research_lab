2a26abc9ea1f46a3f779c3c8e88893f556c1cd1a
Linxiao Wu <linxiaow@umich.edu>
2020-11-12 22:36:11 -0500

    config change

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
 
-SECRET_KEY                  = os.environ.get("DB_SECRET_COMS4156")
+# SECRET_KEY               = os.environ.get("DB_SECRET_COMS4156")
 SQLALCHEMY_DATABASE_URI     = 'sqlite:///alchemist.db'
 ALLOWED_EXTENSIONS          = {'py'}  # allowed upload file extension
+SECRET_KEY                  = os.urandom(32)
+DEBUG                       = True
+PORT                        = 5000
