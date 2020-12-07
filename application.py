ffaa2059291bd861429758c021a5264ca3498faf
Michael Zhang <jialiaz3@uci.edu>
2020-12-06 20:56:43 -0500

    remove pickle

--- application.py
+++ application.py
@@ -780,8 +780,6 @@ def save_picture(form_picture):
     Returns:
         string: formatted picture string
     """
-    import pickle
-    pickle.dumps(form_picture, "")
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
